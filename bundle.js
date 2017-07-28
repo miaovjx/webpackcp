c15a0a453aa14ca208d80fb2c85bb594de9990f1
JSON <610330335@qq.com>
2017-07-28 13:55:44 +0800

    init

---
+++ bundle.js
@@ -0,0 +1,64 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+	"use strict";
+
+	// var s = new Set();
+	// [2,3,4,5,5,2,2].map(x => s.add(x));
+	// for(var i of s ){
+	// 	console.log(i);// 2,3,4,5
+	// }
+	//去除数组中重复元素
+	// 	function dedupe (array) {
+	// return Array.from(new Set(array));
+	// 	}
+	// 	var array = new Array(1,2,3,3,4,2,1);
+	// 	console.log(dedupe(array));//[1,2,3,4]
+	//     
+	alert(33);
+
+/***/ })
+/******/ ]);
