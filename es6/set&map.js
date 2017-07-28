00f3c435393967fdd6a0702f300a8175d11e94fd
JSON <610330335@qq.com>
2017-07-28 17:52:50 +0800

    add index.html

--- es6/set&map.js
+++ es6/set&map.js
@@ -9,5 +9,16 @@
   // 	}
   // 	var array = new Array(1,2,3,3,4,2,1);
   // 	console.log(dedupe(array));//[1,2,3,4]
-  //     
-  alert(33)
+import jquery from './jquery-1.7.2.min.js';   
+import './lottery/base.js';
+import './lottery/timer.js';
+import './lottery/calculate.js';
+import './lottery/interface.js';
+//alert($(window).width());       
+{
+  function test(x, y = 'world'){
+    console.log('默认值',x,y);
+  }
+  test('hello');
+  test('hello','kill');
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,14 @@
 var path = require('path');
 module.exports = {
-  entry: "./es6/set&map.js",
-  output: {
-    path: __dirname,
-    filename: "bundle.js"
-  },
-  module: {
-    loaders: [
-      {
-        test: path.join(__dirname, 'es6'),
-        loader: 'babel-loader'
-      }
-    ]
-  }
+    entry: "./es6/set&map.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [{
+            test: path.join(__dirname, 'es6'),
+            loader: 'babel-loader'
+        }]
+    }
 }
